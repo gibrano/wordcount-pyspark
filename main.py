dbfdc802976df037f1b7a8335cf7d14bd5dda4ef
Gibran Otazo <gibran@entropy.tech>
2019-04-12 14:38:55 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,22 +2,22 @@ from pyspark import SparkContext, SparkConf
 from operator import add
 import time
 
-conf = SparkConf().setAppName("PythonWordCount 1 worker")
+conf = SparkConf().setAppName("PythonWordCount 16 worker")
 sc = SparkContext(conf=conf)
 
 #"https://raw.githubusercontent.com/subpath/ChatBot/master/data/cornell%20movie-dialogs%20corpus/movie_lines.txt"
 file = "movie_lines.txt"
 
-lines = sc.textFile(file, 1)
+lines = sc.textFile(file, 1).collect()
+
+rdd = sc.parallelize(lines)
 
 start = time.time()
 
-counts = lines.flatMap(lambda x: x.split(' ')).map(lambda x: (x, 1)).reduceByKey(add).collect()
+counts = rdd.flatMap(lambda x: x.split(' ')).map(lambda x: (x, 1)).reduceByKey(lambda x,y:x+y).collect()
 
 end = time.time()
-print(end - start)
-
-for (word, count) in counts:
-        print("%s: %i" % (word, count))
+print("time:", end - start)
 
 sc.stop()
+
